e4a39761a4e1cbbcc8ba70ef47363874fa9c93a6
Kaio <trackrules.ksl@gmail.com>
2023-05-23 11:08:37 +1200

    inclass

--- ex4_q1.s
+++ ex4_q1.s
@@ -3,13 +3,15 @@
 
 main:
     add $3, $0, $0 
-    lw $3, 0x73009($0)
     addi $5, $5, 1
 
     movsg $2, $cctrl
     andi $2, $2, 0x000f
-    ori $2, $2, 0x22
+    ori $2, $2, 0xA2
     movgs $cctrl, $2 
+
+    addi $7, $0, 0x3 
+    sw $7, 0x73004($0)
 
     movsg $2, $evec         #Copy the old handler’s address to $2
     sw $2, old_handler($0)  #Save it to memory
@@ -18,7 +20,7 @@ main:
 
 loop:
     sw $3, 0x73009($0)
-    subi $4, $3, 0xA
+    subi $4, $3, 0x000A
     bnez $4, loop
     addi $5, $5, 1
     sw $5, 0x73008($0)
@@ -29,7 +31,11 @@ handler:
     movsg $13, $estat       #Get the value of the exception status register
     andi $13, $13, 0xffd0   #Check if interrupt we don’t handle ourselves
     beqz $13, handle_irq1   #If it one of ours, go to our handler
-    
+
+    movsg $13, $estat       #Get the value of the exception status register
+    andi $13, $13, 0xff70   #Check if interrupt we don’t handle ourselves
+    beqz $13, handle_pp     #If it one of ours, go to our handler
+
     lw $13, old_handler($0) #Otherwise, jump to the default handler
     jr $13                  #That we saved earlier.
 
@@ -37,6 +43,18 @@ handle_irq1:
     addi $3, $3, 1          #Handle our interrupt
     sw $0, 0x7f000($0)      #Acknowledge the interrupt
     rfe 
+
+handle_pp:
+    lw $13, 0x73005($0)
+    beqz $13, handle_pp_exit
+    addi $3, $3, 1          #Handle our interrupt
+    sw $13, 0x73005($0)      #Acknowledge the interrupt
+
+
+handle_pp_exit:
+    sw $0, 0x73005($0) 
+    rfe 
+
 
 .data
 old_handler:
