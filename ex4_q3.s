707b2f0cd4bdad189888cbd602c2ce43d4db1d83
Kaio <trackrules.ksl@gmail.com>
2023-05-24 12:18:05 +1200

    q3 start

--- ex4_q3.s
+++ ex4_q3.s
@@ -6,7 +6,7 @@ main:
 
     movsg $2, $cctrl        #Get val of cctrl
     andi $2, $2, 0x000f     #Disable interrupts
-    ori $2, $2, 0x42        #Enable irq2
+    ori $2, $2, 0xC2        #Enable irq2
     movgs $cctrl, $2        #store back in cctrl
 
     sw $0, 0x72003($0)      # Make sure there are no old interrupts still hanging around
@@ -14,6 +14,8 @@ main:
     sw $11, 0x72001($0)
     addi $11, $0, 0x3       # Enable the timer and autorestart
     sw $11, 0x72000($0)
+    # addi $2, $0, 0x3      #Not sure if this is needed
+    # sw $2, 0x73004($0)
 
     movsg $2, $evec         #Copy the old handler’s address to $2
     sw $2, old_handler($0)  #Save it to memory
@@ -33,6 +35,10 @@ j loop
 
 handler:
     movsg $13, $estat       #Get the value of the exception status register
+    andi $13, $13, 0xff70   #Check if interrupt we don’t handle ourselves
+    beqz $13, handle_pp     #If it one of ours, go to our handler
+
+    movsg $13, $estat       #Get the value of the exception status register
     andi $13, $13, 0xffb0   #Check if interrupt we don’t handle ourselves
     beqz $13, handle_irq2   #If it one of ours, go to our handler
 
@@ -43,6 +49,31 @@ handle_irq2:
     addi $3, $3, 1          #Handle our interrupt
     sw $0, 0x72003($0)      #Acknowledge the interrupt
     rfe 
+    
+handle_pp:
+    lw $13, 0x73001($0)
+    beqz $13, handle_pp_exit
+
+    subi $4, $13, 0x1
+    beqz $4, startstop
+    
+    andi $4, $13, 0x2
+    bnez $4, resetWhenOff
+    
+    andi $4, $13, 0x4
+    bnez $4, end            #terminate
+
+handle_pp_exit:
+    sw $0, 0x73005($0)      #Acknowledge the interrupt
+    rfe 
+startstop:
+    # lw $13, 0x72001($0)
+    addi $13, $0, 0x0
+    # andi $13, $13, 0x3
+    sw $13, 0x72001($0)
+    j handle_pp_exit
+
+resetWhenOff:
 
 end:
 
